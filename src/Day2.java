401fabb818dfec580456abb23f29859853d5cf3a
Ruslan Gilmutdinov <gilmutdinov.ruslan@gmail.com>
2022-12-02 13:32:55 +0600

    Remove unused files

--- src/Day2.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-import java.util.Scanner;
-
-public class Day2 {
-    public void solve() throws Exception {
-        File file = new File("data\\day2.txt");
-        Scanner scanner = new Scanner(file);
-    }
-}
