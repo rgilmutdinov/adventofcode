088f89abea8c22360254ddcae5f76a1eb1fb20af
Ruslan Gilmutdinov <gilmutdinov.ruslan@gmail.com>
2022-12-17 02:35:01 +0600

    Add extra solutions for Day 16

--- src/year2022/days/Day16.java
+++ src/year2022/days/Day16.java
@@ -118,4 +118,160 @@ public class Day16 extends Day2022 {
         return cache.get(valve)[timeLeft][mask];
     }
 
+
+    public Object solvePart1_ex() {
+        Scanner scanner = getInputScanner();
+        Map<String, List<String>> adjMap = new LinkedHashMap<>();
+        Map<String, Integer> rates = new LinkedHashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            Matcher matcher = pattern.matcher(s);
+            if (matcher.find()) {
+                String valve = matcher.group("valve");
+                List<String> target = Arrays.stream(matcher.group("to")
+                        .split(",")).map(String::trim).toList();
+
+                int rate = Integer.parseInt(matcher.group("rate"));
+
+                adjMap.put(valve, target);
+                rates.put(valve, rate);
+            }
+        }
+
+        var addDists = floydWarshall(adjMap);
+        Map<String, Map<String, Integer>> dists = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
+        for (String v : addDists.keySet()) {
+            Map<String, Integer> to = new HashMap<>();
+            for (String w : addDists.get(v).keySet()) {
+                if (rates.getOrDefault(w, 0) > 0) {
+                    to.put(w, addDists.get(v).get(w));
+                }
+            }
+            if (rates.get(v) > 0 || v.equals("AA")) {
+                idxMap.put(v, idxMap.size());
+                dists.put(v, to);
+            }
+        }
+
+        int allMask = (1 << idxMap.size()) - 1;
+        Integer[][][] cache = new Integer[idxMap.size()][31][allMask + 1];
+        return dfs("AA", allMask, 30, dists, rates, idxMap, cache);
+    }
+
+    public Object solvePart2_ex() {
+        Scanner scanner = getInputScanner();
+        Map<String, List<String>> adjMap = new HashMap<>();
+        Map<String, Integer> rates = new HashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            Matcher matcher = pattern.matcher(s);
+            if (matcher.find()) {
+                String valve = matcher.group("valve");
+                List<String> target = Arrays.stream(matcher.group("to")
+                        .split(",")).map(String::trim).toList();
+
+                int rate = Integer.parseInt(matcher.group("rate"));
+
+                adjMap.put(valve, target);
+                rates.put(valve, rate);
+            }
+        }
+
+        var addDists = floydWarshall(adjMap);
+        Map<String, Map<String, Integer>> dists = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
+        for (String v : addDists.keySet()) {
+            Map<String, Integer> to = new HashMap<>();
+            for (String w : addDists.get(v).keySet()) {
+                if (rates.getOrDefault(w, 0) > 0) {
+                    to.put(w, addDists.get(v).get(w));
+                }
+            }
+            if (rates.get(v) > 0 || v.equals("AA")) {
+                idxMap.put(v, idxMap.size());
+                dists.put(v, to);
+            }
+        }
+
+        int allMask = (1 << idxMap.size()) - 1;
+        Integer[][][] cache = new Integer[idxMap.size()][27][allMask + 1];
+
+        int best = 0;
+        for (int mask = 0; mask <= allMask; mask++) {
+            int you = dfs("AA", mask, 26, dists, rates, idxMap, cache);
+            int elephant = dfs("AA", allMask ^ mask, 26, dists, rates, idxMap, cache);
+            best = Math.max(best, you + elephant);
+        }
+
+        return best;
+    }
+
+    private int dfs(String u, int mask, int time,
+                    Map<String, Map<String, Integer>> dists,
+                    Map<String, Integer> rates,
+                    Map<String, Integer> idxMap,
+                    Integer[][][] cache) {
+        int best = 0;
+        Map<String, Integer> dist = dists.get(u);
+
+        int id = idxMap.get(u);
+        if (cache[id][time][mask] == null) {
+            for (String v : dist.keySet()) {
+                int bit = 1 << idxMap.get(v);
+                int dv = dist.get(v);
+                if ((mask & bit) > 0 && time - dv - 1 >= 0) {
+                    best = Math.max(best, (time - dv - 1) * rates.get(v) + dfs(
+                            v,
+                            mask ^ bit,
+                            time - dv - 1,
+                            dists,
+                            rates,
+                            idxMap,
+                            cache
+                    ));
+                }
+            }
+
+            cache[id][time][mask] = best;
+        }
+
+        return cache[id][time][mask];
+    }
+
+    private static final int MAX = 1_000_000_000;
+
+    public Map<String, Map<String, Integer>> floydWarshall(Map<String, List<String>> adjMap) {
+        Map<String, Map<String, Integer>> dists = new HashMap<>();
+
+        List<String> valves = new ArrayList<>(adjMap.keySet());
+        for (String v : valves) {
+            Map<String, Integer> dist = new HashMap<>();
+            for (String u : adjMap.get(v)) {
+                dist.put(u, 1);
+            }
+            dists.put(v, dist);
+        }
+
+        for (String w : valves) {
+            for (String v : valves) {
+                if (!dists.get(w).containsKey(v)) {
+                    continue;
+                }
+
+                for (String u : valves) {
+                    int vw = dists.get(v).getOrDefault(w, MAX);
+                    int wu = dists.get(w).getOrDefault(u, MAX);
+                    int vu = dists.get(v).getOrDefault(u, MAX);
+
+                    dists.get(v).put(u, Math.min(vu, vw + wu));
+                }
+            }
+        }
+
+        return dists;
+    }
 }
+
