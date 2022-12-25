d492db613abef13829380d6d7662bbf9b0a0937a
Ruslan Gilmutdinov <gilmutdinov.ruslan@gmail.com>
2022-12-25 12:03:44 +0600

    Add Day 25 solutions

--- src/year2022/days/Day25.java
+++ src/year2022/days/Day25.java
@@ -2,6 +2,9 @@ package year2022.days;
 
 import year2022.Day2022;
 
+import java.util.Map;
+import java.util.Scanner;
+
 public class Day25 extends Day2022 {
     public Day25() {
         super(25);
@@ -9,11 +12,59 @@ public class Day25 extends Day2022 {
 
     @Override
     public Object solvePart1() {
-        return 0;
+        Scanner scanner = getInputScanner();
+        long sum = 0;
+        while (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            sum += fromSNAFU(s);
+        }
+
+        return toSNAFU(sum);
+    }
+
+    private static final Map<Character, Integer> DIGITS = Map.of(
+        '2', 2,
+        '1', 1,
+        '0', 0,
+        '-', -1,
+        '=', -2
+    );
+
+    private static final Map<Integer, Character> DIGITS_REV = Map.of(
+        2, '2',
+        1, '1',
+        0, '0',
+        -1, '-',
+        -2, '='
+    );
+
+    private long fromSNAFU(String s) {
+        long n = 0;
+        for (int i = 0; i < s.length(); i++) {
+            n = 5L * n + (DIGITS.get(s.charAt(i)));
+        }
+        return n;
+    }
+
+    private String toSNAFU(long n) {
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int digit = (int) (n % 5);
+            n /= 5;
+
+            if (digit >= 3) {
+                digit -= 5;
+                n++;
+            }
+
+            sb.append(DIGITS_REV.get(digit));
+        }
+
+        return sb.reverse().toString();
     }
 
     @Override
     public Object solvePart2() {
-        return 0;
+        return "No part 2";
     }
 }
