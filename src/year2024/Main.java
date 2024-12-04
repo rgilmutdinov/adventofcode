78ed1f619efc8e353fe70824254ff2d3134bd4cc
Ruslan Gilmutdinov <gilmutdinov.ruslan@gmail.com>
2024-12-04 01:09:24 +0100

    Add day 3 solutions

--- src/year2024/Main.java
+++ src/year2024/Main.java
@@ -1,13 +1,13 @@
 package year2024;
 
 import common.Day;
-import year2024.days.Day03;
+import year2024.days.Day04;
 
 import java.util.concurrent.Callable;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Day problem = new Day03();
+        Day problem = new Day04();
         measure(problem::solvePart1);
         measure(problem::solvePart2);
     }
--- src/year2024/days/Day03.java
+++ src/year2024/days/Day03.java
@@ -3,8 +3,13 @@ package year2024.days;
 import year2024.Day2024;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Day03 extends Day2024 {
+    private static final Pattern PATTERN_MUL = Pattern.compile("mul\\((?<num1>\\d{1,3}),(?<num2>\\d{1,3})\\)");
+    private static final Pattern PATTERN_ALL = Pattern.compile("do\\(\\)|don't\\(\\)|mul\\((?<num1>\\d{1,3}),(?<num2>\\d{1,3})\\)");
+
     public Day03() {
         super(3);
     }
@@ -12,12 +17,42 @@ public class Day03 extends Day2024 {
     @Override
     public Object solvePart1() {
         Scanner scanner = getInputScanner();
-        return null;
+        long total = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Matcher match = PATTERN_MUL.matcher(line);
+            while (match.find()) {
+                long num1 = Long.parseLong(match.group("num1"));
+                long num2 = Long.parseLong(match.group("num2"));
+                total += num1 * num2;
+            }
+        }
+        return total;
     }
 
     @Override
     public Object solvePart2() {
         Scanner scanner = getInputScanner();
-        return null;
+        boolean enabled = true;
+        long total = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            Matcher match = PATTERN_ALL.matcher(line);
+            while (match.find()) {
+
+                String s = match.group();
+                if (s.equals("do()")) {
+                    enabled = true;
+                } else if (s.equals("don't()")) {
+                    enabled = false;
+                } else if (enabled) {
+                    long num1 = Long.parseLong(match.group("num1"));
+                    long num2 = Long.parseLong(match.group("num2"));
+                    total += num1 * num2;
+                }
+            }
+        }
+        return total;
     }
 }
